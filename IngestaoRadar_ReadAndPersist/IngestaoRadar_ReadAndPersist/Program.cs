0407232724120b05e70a30e63f8f7cecd2dbf1ce
EnzoHenrico <enzo.mascia1@gmail.com>
2024-06-02 13:32:19 -0300

    fix: Adequacoes do programa para mvc e responsabilidade de database

--- IngestaoRadar_ReadAndPersist/IngestaoRadar_ReadAndPersist/Program.cs
+++ IngestaoRadar_ReadAndPersist/IngestaoRadar_ReadAndPersist/Program.cs
@@ -1,70 +1,14 @@
-﻿using Models;
-using MongoDB.Bson;
-using System.Data.SqlClient;
+﻿using Controllers;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        var controller = new ReadAndPersistControllers();
 
         Console.WriteLine("Read and Persist");
-        InsertIntoMongo();
-    }
+        var succes = controller.MigrateData();
 
-    public static void InsertIntoMongo()
-    {
-        var client = MongoDatabase.GetInstance();
-        var db = client.Connection.GetDatabase("Radar");
-        var collection = db.GetCollection<BsonDocument>("Data");
-        db.DropCollection("Data");
-
-        var listDocument = new List<BsonDocument>();
-        foreach (var item in GetSQLRecordsList())
-        {
-            var document = new BsonDocument { item.GetBsonDocument() };
-            listDocument.Add(document);
-        }
-        collection.InsertMany(listDocument);
-    }
-    public static List<Radar> GetSQLRecordsList()
-    {
-        List<Radar> lst = new List<Radar>();
-        var _sql = MsSqlDatabase.GetInstance();
-        SqlCommand cmdSQL = new SqlCommand();
-
-        string aux = " SELECT [concessionaria], [ano_do_pnv_snv], [tipo_de_radar], [rodovia], [uf], " +
-                     "[km_m], [municipio], [tipo_pista], [sentido], [situacao], " +
-                     "[data_da_inativacao], [latitude], [longitude], [velocidade_leve] " +
-                     "FROM [dbRadar].[dbo].[RadarData] ";
-
-        _sql.Connection.Open();
-        cmdSQL.CommandText = aux;
-        cmdSQL.Connection = _sql.Connection;
-        try
-        {
-            using (SqlDataReader reader = cmdSQL.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    List<string> dataInativacao = new List<string>();
-
-                    var temp = reader.GetString(10).Split(',');
-                    foreach (var item in temp)
-                        dataInativacao.Add(item);
-
-                    Radar obj = new Radar(reader.GetString(0), reader.GetInt32(1), reader.GetString(2), reader.GetString(3), reader.GetString(4),
-                                      reader.GetString(5), reader.GetString(6), reader.GetString(7), reader.GetString(8), reader.GetString(9),
-                                      dataInativacao, reader.GetString(11), reader.GetString(12), reader.GetInt32(13));
-
-                    lst.Add(obj);
-                }
-            }
-        }
-        catch (SqlException)
-        {
-            throw;
-        }
-        _sql.Connection.Close();
-        return lst;
+        Console.WriteLine($"{(succes ? "Operação realizada com sucesso!" : "Falha na operação...")}");
     }
 }
---
+++ IngestaoRadar_ReadAndPersist/Services/ReadAndPersistServices.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.SqlClient;
+using Models;
+using System.Text.Json;
+using MongoDB.Bson;
+using Repositories;
+
+namespace Services
+{
+    public class ReadAndPersistServices
+    {
+        private readonly ReadAndPersistRepositories _repositories = new();
+
+        public bool MigrateDataFromSqlToMongo()
+        {
+            var result = false;
+            try
+            {
+                // Read from SQL
+                var sqlData = _repositories.SelectAllSql();
+
+                // Convert to Bson
+                var listDocument = new List<BsonDocument>();
+                foreach (var item in sqlData)
+                {
+                    var document = new BsonDocument
+                    {
+                        item.GetBsonDocument()
+                    };
+                    listDocument.Add(document);
+                }
+
+                // Write in Mongo
+                _repositories.InsertManyMongoDb(listDocument);
+
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
